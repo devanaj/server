7c88f8f2ca562b1d1728aa27206f7fb83347e1a1
devanaj <137056141+devanaj@users.noreply.github.com>
2026-01-12 14:36:47 +0530

    changes

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,10 @@
+/* This code snippet is setting up a basic route in a Node.js application using the Express framework.
+Here's a breakdown of what each part does: */
 const express = require("express")
 const router = express.Router()
 
 router.get("/",(req,res)=>{
-    res.send("home page")
+    res.send("home pageuuuuuuuuuuuuuuu")
 })
 
 module.exports=router
